26e7e119c167e8bd911214497c27dda14c3827a6
Timothy Scheuering <timsch@cisco.com>
2023-08-04 13:19:30 -0700

    feat(components/text): make new text component

--- packages/@momentum-design/components/src/components/text/text.styles.ts
+++ packages/@momentum-design/components/src/components/text/text.styles.ts
@@ -1,63 +1,20 @@
 import { css } from 'lit';
 
 const styles = css`
-  :host {
-    color: var(--mds-color-theme-text-primary-normal);
-    font-family: Inter;
-  }
+:host {
+  display: inline;
+}
 
-  .mdc-text-h1 {
-    font-size: 7.5rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 125%; /* 9.375rem */
-    text-transform: capitalize;
-  }
+:host([role="paragraph"]) {
+  display: block;
+}
 
-  .mdc-text-h2 {
-    font-size: 6rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 125%; /* 7.5rem */
-    text-transform: capitalize;
-  }
-
-  .mdc-text-h3 {
-    font-size: 3rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 125%; /* 3.75rem */
-  }
-
-  .mdc-text-h4 {
-    font-size: 2.5rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 125%; /* 3.125rem */
-  }
-
-  .mdc-text-h5 {
-    font-size: 2rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 125%; /* 2.5rem */
-    letter-spacing: 0.5rem;
-    text-transform: uppercase;
-  }
-
-  .mdc-text-h6 {
-    font-size: 1.75rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 3rem; /* 171.429% */
-  }
-
-  .mdc-text-h7 {
-    font-size: 1.5rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 140%; /* 2.1rem */
-  }
+:host([aria-level="1"][role="heading"]) {}
+:host([aria-level="2"][role="heading"]) {}
+:host([aria-level="3"][role="heading"]) {}
+:host([aria-level="4"][role="heading"]) {}
+:host([aria-level="5"][role="heading"]) {}
+:host([aria-level="6"][role="heading"]) {}
 `;
 
-export { styles };
+export default styles;
