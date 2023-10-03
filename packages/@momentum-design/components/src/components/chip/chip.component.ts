371b432f9855d6bb2ecf50eef78afebfc5cf1174
Christoph Scheffauer <scheffauerc@gmail.com>
2023-10-03 21:07:04 +0200

    fix: linting in chip

--- packages/@momentum-design/components/src/components/chip/chip.component.ts
+++ packages/@momentum-design/components/src/components/chip/chip.component.ts
@@ -27,40 +27,42 @@ class Chip extends MaterialChip {
     const { ariaLabel } = this as ARIAMixinStrict;
     if (this.href) {
       return html`
-        <a class="primary action"
+        <a
+          class="primary action"
           id="link"
           aria-label=${ariaLabel || nothing}
           href=${this.href}
           target=${this.target || nothing}
-        >${content}</a>
+          >${content}</a
+        >
       `;
     }
 
     return html`
-      <button class="primary action"
+      <button
+        class="primary action"
         id="button"
         aria-label=${ariaLabel || nothing}
         ?disabled=${this.disabled && !this.alwaysFocusable}
         type="button"
-      >${content}</button>
+      >
+        ${content}
+      </button>
     `;
   }
 
   protected renderSomething() {
     return html`
-    <span class="leading icon" aria-hidden="true">
-      ${this.renderLeadingIcon()}
-    </span>
-    <span class="label">${this.label}</span>
-    <span class="touch"></span>
-  `;
+      <span class="leading icon" aria-hidden="true"> ${this.renderLeadingIcon()} </span>
+      <span class="label">${this.label}</span>
+      <span class="touch"></span>
+    `;
   }
 
   protected override renderContainerContent() {
     return html`
     ${this.renderOutline()}
-    <mdc-focus-ring part="focus-ring"
-        for=${this.primaryId}></md-focus-ring>
+    <mdc-focus-ring part="focus-ring" for=${this.primaryId}></mdc-focus-ring>
     ${this.renderPrimaryAction(this.renderSomething())}
   `;
   }
