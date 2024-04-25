c92c4d065f2caea7311b409536a601f7aaf4c68e
Christoph Scheffauer <scheffauerc@gmail.com>
2024-04-25 15:08:35 +0200

    feat: finished header

--- packages/@momentum-design/vscode-extension/src/components/Header.tsx
+++ packages/@momentum-design/vscode-extension/src/components/Header.tsx
@@ -1,7 +1,14 @@
 import './Header.css';
+import { Text } from '@momentum-ui/react-collaboration';
+
+// @ts-ignore: next-line
+import Logo from "@momentum-design/brand-visuals/dist/svg/momentum-design-logo-dark-color-horizontal.svg?react";
 
 export const Header = () => {
   return (
-    <div className="headerWrapper">Header</div>
+    <div className="headerWrapper">
+        <Text type="header-primary" className='headerText'>Momentum Tank</Text>
+        <Logo className="headerLogo"/>
+    </div>
   );
 };
--- packages/@momentum-design/vscode-extension/vite.config.ts
+++ packages/@momentum-design/vscode-extension/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,7 +15,7 @@ export default defineConfig({
     open: true,
     port: 5000,
   },
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     // Workaround to fix inline dependency of a dependency, which is the case in @momentum-ui/react-collaboration
     mainFields: ['module'],
