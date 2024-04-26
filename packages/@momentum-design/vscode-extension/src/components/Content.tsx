35b2dac21c00fe01cedcd15c9bd447a8ed0c36d6
James Nestor <jnestor@cisco.com>
2024-04-26 11:25:26 +0100

    feat: add lottie player and annimations placeholder

--- packages/@momentum-design/vscode-extension/src/components/Content.tsx
+++ packages/@momentum-design/vscode-extension/src/components/Content.tsx
@@ -1,8 +1,8 @@
-import { tabData } from '../constants.tsx';
-import './Content.css';
+import { tabData } from "../constants.tsx";
+import "./Content.css";
 
 interface ContentProps {
-  activeTabId: string
+  activeTabId: string;
 }
 
 export const Content = ({ activeTabId }: ContentProps) => {
@@ -11,24 +11,4 @@ export const Content = ({ activeTabId }: ContentProps) => {
       {tabData.map((tabDataEntry) => activeTabId === tabDataEntry.id && tabDataEntry.content)}
     </div>
   );
-};
-
-export const IconsTab = () => {
-  return <div>Icons Tab</div>;
-};
-
-export const IllustrationsTab = () => {
-  return <div>Illustrations Tab</div>;
-};
-
-export const TokensTab = () => {
-  return <div>Tokens Tab</div>;
-};
-
-export const BrandVisualsTab = () => {
-  return <div>Brand Visuals Tab</div>;
-};
-
-export const AnnimationsTab = () => {
-  return <div>Annimations Tab</div>;
 };
--- packages/@momentum-design/vscode-extension/src/constants.tsx
+++ packages/@momentum-design/vscode-extension/src/constants.tsx
@@ -1,35 +1,36 @@
+import { AnnimationsContent } from "./components/contents/Annimations";
 import { IconsContent } from "./components/contents/Icons";
 
-export const tabData: { id: string; name: string; icon: string, content: React.ReactNode }[] = [
+export const tabData: { id: string; name: string; icon: string; content: React.ReactNode }[] = [
   {
     id: "icons",
     name: "Icons",
     icon: "widget",
-    content: <IconsContent/>
+    content: <IconsContent />,
   },
   {
     id: "illustrations",
     name: "Illustrations",
     icon: "colour-palette",
-    content: <p style={{marginLeft: '24px'}}>Coming soon...</p>
+    content: <p style={{ marginLeft: "24px" }}>Coming soon...</p>,
   },
   {
     id: "tokens",
     name: "Tokens",
     icon: "fill-colour",
-    content: <p style={{marginLeft: '24px'}}>Coming soon...</p>
+    content: <p style={{ marginLeft: "24px" }}>Coming soon...</p>,
   },
   {
     id: "brand-visuals",
     name: "Brand visuals",
     icon: "webex-helix",
-    content: <p style={{marginLeft: '24px'}}>Coming soon...</p>
+    content: <p style={{ marginLeft: "24px" }}>Coming soon...</p>,
   },
   {
     id: "animations",
     name: "Animations",
     icon: "video-effect",
-    content: <p style={{marginLeft: '24px'}}>Coming soon...</p>
+    content: <AnnimationsContent />,
   },
 ];
 
